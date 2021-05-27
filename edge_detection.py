b4efa78db96de96c336dda18ea51003172a0d2d9
saeedtaghavi <saeed.taghavi.v@gmail.com>
2021-05-27 08:46:03 +0430

    add condition to handle 2D-BW images and 2D-RGB images

--- edge_detection.py
+++ edge_detection.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# -- coding: utf-8 --
 """
 Created on Wed May 26 13:11:14 2021
 
@@ -15,20 +15,25 @@ img = mpimg.imread('./img/sample0.jpg')
 # Output Images
 plt.imshow(img)
 #get image dimensions
+
 print(np.shape(img))
-height, width , depth = np.shape(img)
 
-# seprate different layers
-R,G,B = img[:,:,0],img[:,:,1],img[:,:,2]
+print(img.ndim)
+if (img.ndim==2):
+    img=img
+elif(img.ndim==3):
+    height, width , depth = np.shape(img)
 
-
+    # seprate different layers
+    R,G,B = img[:,:,0],img[:,:,1],img[:,:,2]
+    img =R
 
 from scipy.ndimage import convolve
 from scipy.ndimage import gaussian_filter
 
 #smoth image with gaussian_filter
-result = gaussian_filter(R, sigma=1)
-
+result = gaussian_filter(img, sigma=2)
+# result =img
 #define different kernels
 kernelLaplacian =[[0,-1,0],[-1,4,-1],[0,-1,0]]
 kernelSobely =[[1,2,1],[0,0,0],[-1,-2,-1]]
@@ -37,4 +42,3 @@ kernelSobelx = [[1,0,-1],[2,0,-2],[1,0,-1]]
 #edge detection using a kernel
 new = convolve(result, kernelLaplacian)
 plt.imshow(new)
-
